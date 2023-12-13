0194d71b9a1d4f7456504d9908a121e85633ed3c
Felix Ouma <moxiemeyoh@gmail.com>
2023-12-13 13:16:59 +0300

    Add: Contstants.js
    -a file to manage all the constant values of the web page.
    -enable easy tracking and changing the variables when need arise

---
+++ src/assets/constants.js
@@ -0,0 +1,45 @@
+import Albert from "./../assets/Albert portrait.jpg";
+import Calvin from "./../assets/calvin portrait.jpg";
+import Kuria from "./../assets/felix portrait.jpg";
+
+export const leaders = [
+    {
+        id: 1,
+        name: "Shibiriti Calvin",
+        role: "Chairman",
+        image: Calvin
+    },
+    {
+        id: 2,
+        name: "Isaac Kuria",
+        role: "Secretary",
+        image: Kuria
+    },
+    {
+        id: 13,
+        name: "Albert Mwenga",
+        role: "Treasurer",
+        image: Albert
+    },
+]
+
+export const values = [
+    {
+        id: 1,
+        value: "friendship",
+        description: "Like the branches of a resilient tree, our friendships intertwine, fostering trust, support, and understanding. We embrace diversity, finding strength in our differences while celebrating our commonalities. Each member contributes a unique hue to the vibrant tapestry of our bond, enriching our lives in countless ways.",
+        icon: " icon for friendship"
+    },
+    {
+        id: 2,
+        value: "growth",
+        description: "We foster a culture where learning is perpetual, mistakes are stepping stones, and challenges are welcomed as opportunities for advancement. Together, we inspire and challenge one another to push boundaries, fostering resilience and adaptability. This shared value of growth fuels our aspirations, empowering us to carve our paths and make a meaningful impact as we navigate the complexities of life as a cohesive unit.",
+        icon: " icon for growth"
+    },
+    {
+        id: 3,
+        value: "service",
+        description: "We embody a collective ethos of empathy and responsibility, recognizing the privilege and duty to uplift others. Through volunteering, mentorship, and philanthropy, we unite our strengths to effect positive change. Our actions exemplify the profound impact of serving others, cultivating a deep sense of fulfillment and purpose. Together, we strive to leave a lasting legacy of compassion and empowerment, knowing that our unity in service transcends individual aspirations, fostering a world where kindness and generosity reign.",
+        icon: " icon for service"
+    },
+]
