e9d3b8cac917d003abc09759ce888fe4d5a7ef9f
Felix Ouma <moxiemeyoh@gmail.com>
2023-12-13 18:51:23 +0300

    Add: new Ibraries hosted from CDN
    -added a google Icon Library
    -the icons are used in the 'Core Values' Section

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -28,18 +28,37 @@ export const values = [
         id: 1,
         value: "friendship",
         description: "Like the branches of a resilient tree, our friendships intertwine, fostering trust, support, and understanding. We embrace diversity, finding strength in our differences while celebrating our commonalities. Each member contributes a unique hue to the vibrant tapestry of our bond, enriching our lives in countless ways.",
-        icon: " icon for friendship"
+        icon: "diversity_3",
     },
     {
         id: 2,
         value: "growth",
         description: "We foster a culture where learning is perpetual, mistakes are stepping stones, and challenges are welcomed as opportunities for advancement. Together, we inspire and challenge one another to push boundaries, fostering resilience and adaptability. This shared value of growth fuels our aspirations, empowering us to carve our paths and make a meaningful impact as we navigate the complexities of life as a cohesive unit.",
-        icon: " icon for growth"
+        icon: "decimal_increase"
     },
     {
         id: 3,
         value: "service",
         description: "We embody a collective ethos of empathy and responsibility, recognizing the privilege and duty to uplift others. Through volunteering, mentorship, and philanthropy, we unite our strengths to effect positive change. Our actions exemplify the profound impact of serving others, cultivating a deep sense of fulfillment and purpose. Together, we strive to leave a lasting legacy of compassion and empowerment, knowing that our unity in service transcends individual aspirations, fostering a world where kindness and generosity reign.",
-        icon: " icon for service"
+        icon: "service_toolbox"
+    }, 
+];
+
+export const navItems = [
+    {
+        title: "Home",
+        path: "/"
+    },
+    {
+        title: "Members",
+        path: "/members"
+    },
+    {
+        title: "Activities",
+        path: "/activities"
+    },
+    {
+        title: "Contact Us",
+        path: "/contact-us"
     },
 ]
