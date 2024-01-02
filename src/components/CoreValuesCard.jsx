07f335a42d17b608311e15485619c3d1ec14fa78
Felix Ouma <moxiemeyoh@gmail.com>
2024-01-02 21:37:15 +0300

    Add : Additional HomePage components - LeaderCard - CoreValuesCard

--- src/components/CoreValuesCard.jsx
+++ src/components/CoreValuesCard.jsx
@@ -4,15 +4,18 @@
 const CoreValuesCard = ({value, description, icon}) => {
   return (
     <article className="mx-4 my-5 text-center">
-        <div className="flex gap-3 items-end ">
-            <span>{icon}</span>
+        <div className="flex gap-3 items-end border-b-8">
+            <span className="material-symbols-outlined">{icon}</span>
             <h4>{value}</h4>
         </div>
-        <p className="ml-4 my-3 text-left">
+        <p className="ml-6 my-3 text-left max-w-md">
             {description}
         </p>
     </article>
   )
 }
 
+<span className="material-symbols-outlined">
+diversity_3
+</span>
 export default CoreValuesCard;
--- src/components/LeaderCard.jsx
+++ src/components/LeaderCard.jsx
@@ -1,15 +1,12 @@
 /* eslint-disable react/prop-types */
 
-
 const LeaderCard = ({name, role, image}) => {
   return (
-    <article className="my-4 mx-2 flex gap-3">
-        <div>
-            <img src={image} alt={name} />
-        </div>
-        <div className="flex flex-col gap-4 text-left">
-            <h1 className="font-Rajdhani my-2 text-3xl">{name}</h1>
-            <h2 className="text-2xl font-kalnia mb-2">{role}</h2>
+    <article className="my-4 mx-2 flex flex-col ss:flex-row gap-3 items-center rounded-br-2xl bg-zinc-600 max-w-md justify-between overflow-x-hidden">
+        <img src={image} alt={name}  className="max-w-[200px] max-h-[300px] rounded-md my-3 mx-3"/>
+        <div className="border-solid border-white mx-4 flex flex-col justify-between items-center">
+          <p>{name}</p>
+          <h2>{role}</h2>
         </div>
     </article>
   )
