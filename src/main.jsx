9ea4a47410746d0be855afaa6bbac55522633a01
Felix Ouma <moxiemeyoh@gmail.com>
2024-01-08 23:55:28 +0300

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider} from "react-redux";
-import store from './app/store';
+// import store from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider >
       <BrowserRouter>
         <App />
       </BrowserRouter>
