da573bfc9dc82c3a49593957cd015d89db5d335f
Felix Ouma <moxiemeyoh@gmail.com>
2024-01-09 23:02:48 +0300

    review: additoinal Activities - more styling and modification on activity page

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -2,7 +2,7 @@
 
 const ActivityCard = ({ title, date, descritpion, image }) => {
   return (
-    <article className="flex flex-col gap-3 shadow-2xl p-2">
+    <article className="flex flex-col gap-3 shadow-2xl p-2 rounded-b-lg">
         <h3 className="text-center font-raleway font-bold text-h3 text-primary">{title}</h3>
         <div className="p-3 flex flex-col  items-center md:flex-row justify-between gap-3 md:items-start">
             <img src={image} alt={title} className="md:max-w-[450px] h-auto rounded-md drop-shadow-xls"/>
@@ -14,7 +14,7 @@ const ActivityCard = ({ title, date, descritpion, image }) => {
         </div>
         <div className="max-w-sm mx-auto p-2 rounded-md shadow-lg z-10 flex flex-center justify-between items-end gap-4">
             <button className="text-white font-raleway font-bold text-p text-center  bg-bgButton p-2 rounded-md">{date}</button>
-            <button className="p-2 bg-bgButtonSecondary text-white rounded-md text-p text-center font-raleway font-bold">Read More</button>
+            <button className="p-2 bg-bgButtonSecondary text-white rounded-md text-p text-center font-raleway font-bold">View Gallary</button>
         </div>
     </article>
   )
--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { activities } from "../assets/constants";
 import ActivityCard from "../components/ActivityCard";
 
@@ -6,8 +7,11 @@ const Activities = () => {
   return (
     <section className="mt-28 flex flex-col items-center gap-4 w-5/6 mx-auto mb-5">
       <h2 className="pt-10 text-center text-h2 font-raleway font-bold">Our Activities</h2>
-      <p className="text-p text-center font-Oswald font-medium max-w-md">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quibusdam inventore cum et quaerat, facilis voluptate quis facere, eos est earum quo. Quaerat sit, labore laboriosam a est qui ratione.
+      <p className="text-p text-center font-Oswald font-medium max-w-lg">
+        We are a collective of individuals dedicated to cultivating strong bonds, enriching our community, and nurturing the spirit of friendship through a diverse array of annual activities.
+      </p>
+      <p className="text-p text-center font-Oswald font-medium max-w-lg">
+        Our calendar brims with initiatives that encapsulate the essence of giving back to the community. Furthermore, within the sanctum of our club, each member's journey is cherished and celebrated. We understand the importance of individual growth and fulfillment.
       </p>
       <div className="flex flex-col items-start gap-6">
         {
