561a631213898320bfe7cccd932e6ef389caffeb
Felix Ouma <moxiemeyoh@gmail.com>
2024-01-09 23:01:22 +0300

    Add: More static files to the project

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,6 +1,17 @@
 import fun from "../assets/fun-illustration.jpg"
 import empathy from "../assets/empathy-illustration.jpg";
+
 import ronnie from "../assets/bwire.jpg";
+import mandela from "../assets/mandela.jpg";
+import gachanja from "../assets/Gachanja.jpg";
+import izzo from "../assets/kuria.jpg";
+import omu from "../assets/omu.jpg";
+import stedy from "../assets/rolland.jpg";
+import Shibiriti from "../assets/shibi.jpg";
+import Mwenga from "../assets/alberto.jpg";
+import Mukaka from "../assets/mukaka.jpg";
+import felloh from "../assets/felloh.jpg";
+
 import growth from "../assets/growth-illustration.jpg";
 import resilience from "../assets/resilience-illlustration.jpg";
 import team from "../assets/team-work.jpg";
@@ -9,6 +20,9 @@ import social from "../assets/social -illustration.jpg";
 import adventure from "../assets/adventure illustration.jpg";
 import kanjataCamping from "../assets/kianjata-Camping.jpg";
 import SomaSometo from "../assets/Soma-Someto.jpg";
+import bloodDrive from "../assets/../assets/Mathari-blood-drive.jpg";
+import UpendoHome from "../assets/../assets/upendo.jpg";
+import hike from "../assets/../assets/hike.jpg";
 
 
 export const leaders = [
@@ -16,19 +30,19 @@ export const leaders = [
         id: 1,
         name: "Shibiriti Calvin",
         role: "Chairman",
-        image: ronnie
+        image: Shibiriti
     },
     {
         id: 2,
         name: "Isaac Kuria",
         role: "Secretary",
-        image: ronnie
+        image: izzo
     },
     {
         id: 13,
         name: "Albert Mwenga",
         role: "Treasurer",
-        image: ronnie
+        image: Mwenga
     },
 ]
 
@@ -87,65 +101,65 @@ export const  members = [
         id: 1,
     },
     {
-        name: "Felx Meyoh",
+        name: "Felix Ouma",
         Profession: "Member",
-        image: ronnie,
+        image: felloh,
         comments: "A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to various workout needs.",
         id: 13,
     },
     {
         name: "Albert Memnga",
         Profession: "Member",
-        image: ronnie,
+        image: Mwenga,
         comments: " A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to and suspension trainers, promoting dynamic exercises and core strength development. A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to and suspension trainers, promoting dynamic exercises and core strength development.",
         id: 17,
     },
     {
         name: "Kuria Isaac",
         Profession: "Member",
-        image: ronnie,
+        image: izzo,
         comments: "A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to various workout needs, gyms often offer promoting dynamic exercises and core strength development.",
         id: 15,
     },
     {
         name: "Mandela Titus",
         Profession: "Member",
-        image: ronnie,
+        image: mandela,
         comments: "A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to various workout needs, gyms often offer promoting dynamic exercises and core strength development.",
         id: 44,
     },
     {
         name: "Stedy Roland",
         Profession: "Member",
-        image: ronnie,
+        image: stedy,
         comments: "A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to various workout needs, gyms often offer promoting dynamic exercises and core strength development.",
         id: 56,
     },
     {
         name: "Mukaka Ben",
         Profession: "Member",
-        image: ronnie,
+        image: Mukaka,
         comments: "A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to various workout needs, gyms often offer promoting dynamic exercises and core strength development.",
         id: 811,
     },
     {
         name: "Gachanja Ephantus",
         Profession: "Member",
-        image: ronnie,
+        image: gachanja,
         comments: "A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to various workout needs, gyms often offer promoting dynamic exercises and core strength development.",
         id: 711,
     },
     {
         name: "Omulindi Kakai",
         Profession: "Member",
-        image: ronnie,
+        image: omu,
         comments: "A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to various workout needs, gyms often offer promoting dynamic exercises and core strength development.",
         id: 511,
     },
     {
         name: "Shibiriti Calvin",
         Profession: "Member",
-        image: ronnie,
+        image: Shibiriti,
         comments: "A modern gym is typically equipped with a range of state-of-the-art fitness equipment catering to various workout needs, gyms often offer promoting dynamic exercises and core strength development.",
         id: 4511,
     },
@@ -196,33 +210,33 @@ export const ourValues = [
 
 export const activities = [
     {
-        title: "Camping at KIanjata",
+        title: "Camping at Kianjata",
         image: kanjataCamping,
         descritpion: "The primary focus of the camping event was to foster bonding and strengthen the camaraderie among the club members. It served as a platform for team-building activities that aimed to deepen the connections between the boys while providing a space for reflection and review of their first year together.  the attendees engaged in various team-building exercises, games, and discussions that allowed them to reflect on their accomplishments, challenges, and growth as a collective over the past year. They shared experiences, exchanged ideas, and strategized for the future, creating an atmosphere of unity and support.",
-        date: "date"
+        date: "2021-12-13"
     },
     {
         title: "SomaSometo Ride",
         image: SomaSometo,
         descritpion: " This journey was not just about cycling; it symbolized a collective effort towards making a difference in the lives of deserving students striving for education. The mission was to raise vital funds to support financially challenged students from Dedan Kimathi University in Nyeri Town, Kenya, in meeting their academic fees. The bike ride embarked from the serene grounds of Dedan Kimathi University, traversing through scenic routes to Menu town and back, covering an impressive estimated distance of 300km. Every pedal stroke during this charitable endeavor was a testament to the unwavering commitment to education and the betterment of these students' futures. It showcased the power of unity and compassion as participants, supporters, and donors came together, propelled by the shared belief in the transformative impact of education.",
-        date: "date"
+        date: "2022-03-21"
     },
     {
         title: "Hiking at Ngong' Hills",
-        image: "adding",
-        descritpion: "l",
-        date: "date"
+        image: hike,
+        descritpion: "Amidst the digital landscape that defined our interactions, the Boys Club embarked on an invigorating hiking expedition to the picturesque Ngong' Hills. This journey wasn't just about conquering peaks; it was a collective endeavor aimed at revitalizing our bonds, nurturing friendship, and reconnecting with nature's beauty. The essence of the day extended beyond physical activity; it was a platform for deep reflection. As we traversed the trails, we engaged in meaningful discussions, reflecting on our recent activities' immediate impact and contemplating our aspirations for the upcoming year, 2023.",
+        date: "2023-03-17"
     },
     {
-        title: "Blood Dvire At Mathari Hospital",
-        image: "adding",
-        descritpion: "l",
-        date: "date"
+        title: "Blood Drive At Mathari Hospital",
+        image: bloodDrive,
+        descritpion: "The 10 Boys Club organized a remarkable blood drive event in collaboration with Mathari Hospital, an initiative born from our collective desire to honor the unsung heroes and heroines in our society. At the heart of this initiative lies our commitment to commemorate Mashujaa Day not merely through words but through actions that reflect the true essence of heroism selfless giving. The response was overwhelming as individuals from all walks of life came forward to contribute to this worthy cause. Through our united efforts, we made a profound impact by donating blood, a precious gift that holds the power to save lives. Each participant became a hero in their own right, embodying the spirit of generosity and compassion that defines the true essence of Mashujaa Day.",
+        date: "2023-10-20"
     },
     {
         title: "Charity Activity At Upendo Home for the Elderly",
-        image: "adding",
-        descritpion: "l",
-        date: "date"
+        image: UpendoHome,
+        descritpion: "The 10 Boys Club, in a harmonious partnership with the vibrant Samba group and the benevolent FIADEHU, orchestrated a heartfelt charity event with a resounding impact. This collaborative effort was geared toward a noble cause raising funds to procure a much-needed dining set for the Upendo Home for the Elderly, nestled in the serene landscapes of Nairutia, within Nyeri County. The impact of this initiative resonated far beyond the confines of the event, leaving an indelible mark on the lives of the elderly residents and fostering a ripple effect of compassion within the community. The shared commitment to enriching the lives of the elderly echoed a profound message of empathy, emphasizing the importance of solidarity in uplifting the underserved and venerable members of society.",
+        date: "2023-10-21",
     },
 ];
